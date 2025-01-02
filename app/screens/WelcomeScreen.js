0b29c38d8a705b51ee26a4c5cea803f66cfb4b66
Lucas Robson <returnnada@gmail.com>
2025-01-02 16:40:18 -0500

    Organized Files, placeholder screen

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground
+            source={require("../assets/favicon.png")}
+            style={styles.background}
+        >
+            <View style={styles.loginButton}></View>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end"
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#4ecdc4'
+    }
+})
+
+export default WelcomeScreen;
