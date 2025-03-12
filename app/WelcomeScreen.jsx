d8896d296a9f872e4bbfc9df97e6879fb4184bb7
Lucas Robson <returnnada@gmail.com>
2025-03-12 01:07:18 -0400

    Updated the welcome screen, adding files for buttons and a loading symbol

--- app/WelcomeScreen.jsx
+++ app/WelcomeScreen.jsx
@@ -1,15 +1,91 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StatusBar, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "../components/ScreenWrapper";
+import { heightPercentage, widthPercentage } from "../helpers/common";
+import { theme } from "../constants/theme";
+import Button from "../components/Button";
 
 const WelcomeScreen = () => {
     return(
-        <ScreenWrapper>
-            <Text>Welcome</Text>
+        <ScreenWrapper bg="white">
+            <StatusBar style="dark"></StatusBar>
+            <View style={styles.container}>
+                <Image style={styles.welcomeImage} 
+                resizeMode="contain" 
+                source={require('../assets/images/Sketchee.png')} />
+
+                <View style={{gap: 20}}>
+                    <Text style={styles.title}>Sketchee</Text>
+                    <Text style={styles.quip}>
+                        A drawing a day keeps the artblock away!
+                    </Text>
+                </View>
+
+                <View style={styles.footer}>
+                    <Button 
+                        title="Getting Started"
+                        buttonStyle={{marginHorizontal: widthPercentage(3)}}
+                        onPress={()=>{}}
+                    />
+                    <View style={styles.bottomTextContainer}>
+                        <Text style={styles.loginText}>
+                            Have an account?
+                        </Text>
+                        <Pressable>
+                            <Text style={[styles.loginText, {color: theme.colors.primaryDark, fontWeight : theme.fonts.semibold}]}>
+                                Login
+                            </Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
         </ScreenWrapper>
     )
 }
 
 export default WelcomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        alignItems : "center",
+        justifyContent : "space-around",
+        backgroundColor : "white",
+
+        paddingHorizontal : widthPercentage(4),
+        // height : heightPercentage(95),
+        // width : widthPercentage(95),
+    },
+    welcomeImage : {
+        height : heightPercentage(30),
+        width : widthPercentage(100),
+        alignSelf : "center",
+    },
+    title : {
+        color : theme.colors.text,
+        fontSize : heightPercentage(4),
+        textAlign : "center",
+        fontWeight : theme.fonts.extraBold
+    },
+    quip : {
+        textAlign : "center",
+        fontSize : heightPercentage(1.7),
+        paddingHorizontal : widthPercentage(10),
+        color : theme.colors.text,
+    },
+    footer : {
+        gap : "30px",
+        width : "100%",
+    },
+    bottomTextContainer : {
+        flexDirection : "row",
+        justifyContent : "center",
+        alignItems : "center",
+        gap : "5px",
+    },
+    loginText : {
+        textAlign : "center",
+        color : theme.colors.text,
+        fontSize : heightPercentage(1.6),
+    },
+})
--- helpers/common.js
+++ helpers/common.js
@@ -2,9 +2,9 @@ import { Dimensions } from "react-native";
 
 const {width: deviceWidth, height: deviceHeight} = Dimensions.get('window');
 
-const heightPercentage = percentage => {
+export const heightPercentage = percentage => {
     return (percentage * deviceHeight) / 100;
 }
-const widthPercentage = percentage => {
+export const widthPercentage = percentage => {
     return (percentage * deviceWidth) / 100;
 }
